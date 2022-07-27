d7599f628f4ee760141b3aee44b9ca903f65dc84
Isabella <isabella.cunha@unesp.br>
2022-07-27 14:03:18 -0300

    React no blog pessoal

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return(
+        <> 
+            <h1>Home</h1> 
+            <img src='https://avatars.githubusercontent.com/u/105821392?v=4' alt ='Imagem Tela inicial' />
+        
+        </>
+
+
+
+    );
+}
+export default Home;
